43bb54f492215243eb72c2ccee77d3637a0cf328
Kushagra Pandey <kushagrajnvgkp@gmail.com>
2023-03-09 09:25:05 +0530

    Ok

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -42,7 +42,79 @@ class _ProfileState extends State<Profile> {
                   Auth().currerntuser!.displayName!,
                   style: const TextStyle(
                       fontSize: 20, fontWeight: FontWeight.bold),
-                )
+                ),
+                const SizedBox(height: 30,),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        width: MediaQuery.of(context).size.width * 0.5 -
+                            50,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            border: Border.all(color: Colors.black)),
+                        child: Column(
+                          children: const [
+                            Icon(Icons.map_outlined, size: 25),
+                            Text('45',
+                                style: TextStyle(
+                                  color: Colors.orange,
+                                  fontSize: 25,
+                                )),
+                            Text('Trips Planned')
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        width: MediaQuery.of(context).size.width * 0.5 -
+                            50,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            border: Border.all(color: Colors.black)),
+                        child: Column(
+                          children: const [
+                            Icon(Icons.currency_rupee_sharp, size: 25),
+                            Text('1,200',
+                                style: TextStyle(
+                                  color: Colors.orange,
+                                  fontSize: 25,
+                                )),
+                            Text('Money Saved')
+                          ],
+                        ),
+                      ),
+                    ]),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      border: Border.all(color: Colors.black)),
+                      child: Column(children: [
+                        Row(
+                            mainAxisAlignment:
+                            MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text("Name"),
+                              Text(Auth().currerntuser!.displayName!)
+                            ]),
+                        Row(
+                            mainAxisAlignment:
+                            MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text("Email"),
+                              Text(Auth().currerntuser!.email!)
+                            ])
+                      ],),
+                ),
+                const SizedBox(height: 30,),
+
               ],
             ),
           ),
